76a18b0cac2e98ab953e6d8c42eff25b7b3a2219
shaquima <senoritabroccolini@gmail.com>
2019-04-26 22:09:27 -0500

    Not-Good-Enough

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,31 +4,54 @@ var wins = 0
 var losses = 0
 var guessesLeft = 5
 var yourGuess = []
+
+
 //make computer choose random letter
-
-
+$( document ).ready(function() {
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 document.onkeyup = function (event) {
     var yourGuess = event.key;
     console.log(yourGuess);
     var letter = event.key.toLowerCase();
+
 }
 
+function wins(){
+    if (yourGuess === computerGuess){
+        wins++; alert("you win")
+    }else{ losses--;
+        aler("you lose")
+    }
+}
+
+// checking computerGuess
 console.log(computerGuess)
 
 document.getElementById("guesses-l").innerText = guessesLeft;
 
+//logging yourGuess on user screen
 document.onkeyup = function(event){
     var letter = event.key;
     yourGuess.push(letter);
+    console.log(yourGuess);
+    for(i = 0; i< yourGuess.length; i++){
+        $("yours").append(yourGuess[i]);
+    }
 
-    for() 
-
-    document.getElementById("yours").innerHTML = letter;
-    var
+    document.getElementById("yours").innerHTML = yourGuess;
 }
 
+document.addEventListener("onkeyup", function(){
+    document.getElementById("demo").innerHTML = "Hello World!";
+  });
 
+
+// document.onkeyup = function(event){
+//     var letter = event.key;
+//     guessesLeft.push()
+// }
+
+});
     
 //     if (userGuess === computerGuess){
 //         document.querySelector("win")
